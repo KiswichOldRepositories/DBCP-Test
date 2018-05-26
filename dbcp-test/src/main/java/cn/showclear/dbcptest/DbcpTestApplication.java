065931bd093a8d3fec1e5feb9d297f67737733c0
Kiswich <zxy16305@gmail.com>
2018-05-26 22:32:04 +0800

    bulid

---
+++ dbcp-test/src/main/java/cn/showclear/dbcptest/DbcpTestApplication.java
@@ -0,0 +1,12 @@
+package cn.showclear.dbcptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbcpTestApplication.class, args);
+    }
+}
